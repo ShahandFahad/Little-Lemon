7b199b2f6e448020e33d37d349eb9b56b2d67915
ShahandFahad <shahandfahadmad@gmail.com>
2023-04-17 14:24:30 +0500

    Adding assets and exporting them from constants

---
+++ src/constants/images.js
@@ -0,0 +1,10 @@
+import logo from '../assets/Logo.svg';
+import greekSalad from '../assets/greekSalad.jpg';
+import resturantFood from '../assets/restauranfood.jpg';
+import lemonDessert from '../assets/lemonDessert.jpg';
+import bruchetta from '../assets/bruchetta.svg';
+import MarioandAdrianA from '../assets/MarioandAdrianA.jpg';
+import MarioandAdrianB from '../assets/MarioandAdrianB.jpg';
+import resturant from '../assets/restaurant.jpg';
+
+export default {logo, greekSalad, resturantFood, lemonDessert, bruchetta, MarioandAdrianA, MarioandAdrianB, resturant};
