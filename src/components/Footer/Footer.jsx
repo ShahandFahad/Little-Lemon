ad42e3b238dcb8904c33e81308d7a25c49fc857f
ShahandFahad <shahandfahadmad@gmail.com>
2023-04-19 12:33:29 +0500

    Coded the footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import './Footer.css';
 import {AppWrap} from '../../wrapper';
+import { images } from "../../constants";
+
+const doormatNavigation = ["Home", "About", "menu", "Reservations", "Order Online", "Login"];
+const contact = ["Adress", "phone number", "email"];
+const socialMedia = ["Facebook", "Instagram", "Twitter", "Youtube"];
+
+const FooterItems = (props) => {
+    return (<div className="app__footer-items">
+        <h1 className="app__footer-item-title">{props.sectionName}</h1>
+        <ul className="app__footer-item-names">
+            {
+                props.sectionList.map((item) => {
+                    return (<li key={item}>
+                        <a href="#">{item}</a>
+                    </li>);
+                })
+            }
+        </ul>
+        
+    </div>);
+};
+
 const Footer = () => {
-    return (<footer>Footer</footer>);
+    return (<footer className="app__footer-section">
+        <div className="app__about-image-box img-box-1"
+            style= 
+            {
+                {
+                    backgroundImage: `url(${images.resturant})`
+                }
+            }
+        ></div>
+        <FooterItems sectionName="Doormat Navigation" sectionList={doormatNavigation} />
+        <FooterItems sectionName="Contact" sectionList={contact} />
+        <FooterItems sectionName="Social Media" sectionList={socialMedia} />
+    </footer>);
 }
 
-export default AppWrap(Footer, 'footer');
+export default AppWrap(Footer, 'footer', 'app__footer');
