acc18b174aebb48b51acf4aed59be9e3bcea4ef0
ShahandFahad <shahandfahadmad@gmail.com>
2023-04-27 22:51:47 +0500

    Updating Header

--- src/components/Reservations/Reservations.jsx
+++ src/components/Reservations/Reservations.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from '../Header/Header';
 function Reservations() {
+    const headerData = {
+        cafeTitle: "Little Lemon",
+        cafeLocation: "Chicago",
+        cafeDescription: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab tempora deleniti eum repellat explicabo ratione quisquam fugiat quidem. Tempora eius deserunt corrupti esse nemo earum, a doloremque minus molestias! Quia.",
+        cafeButtonName: "Reserve a Table",
+    };
+    
   return (
     <div>
-      Reservations
       <Header />
     </div>
   );
